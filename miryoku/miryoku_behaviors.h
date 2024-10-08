f949370baa74fe4558d4175f69eac92ebf29cfd6
anatolytimonin <anatolytimonin8@gmail.com>
2024-10-08 21:08:56 +0200

    refactoring. add macros 'home row mods'

--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -5,3 +5,17 @@
 
 #define U_MT(MOD, TAP) &u_mt MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
+
+#if defined (HRM_ORDER_GASC)
+  #define U_HRM(b1, b2, b3, b4) \
+  U_MT(LGUI, b1), U_MT(LALT, b2), U_MT(LSHFT, b3), U_MT(LCTRL, b4) 
+#else
+  #define U_HRM(b1, b2, b3, b4) \
+  U_MT(LGUI, b1), U_MT(LALT, b2), U_MT(LCTRL, b3), U_MT(LSHFT, b4)
+#endif
+
+#define U_HRM_L(b1, b2, b3, b4) \
+U_HRM(b1, b2, b3, b4)
+
+#define U_HRM_R(b1, b2, b3, b4) \
+U_HRM(b4, b3, b2, b1)
