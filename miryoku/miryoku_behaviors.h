33d222bc4afc0f4071074aafb63039f79d927c34
anatolytimonin <73020261+anatolytimonin@users.noreply.github.com>
2025-12-13 07:54:28 +0200

    Merge corne-choc-gasc into master (#1)
    * 'home row mode' has been replaced by GASC
    
    * added a build for 'corne-choc'
    
    * fixed build for 'corne-choc'
    
    * fixed build for 'corne-choc'
    
    * fixed build for 'corne-choc'
    
    * fixed build for 'corne-choc'
    
    * test add .keymap whis '-'
    
    * rename 'keymap' file
    
    * fixed build for 'corne-choc'
    
    * fixed build for 'corne-choc'
    
    * refactoring. add macros 'home row mods'
    
    * build: enable password entry when device is paired
    
    * build(fix): change the separator
    
    * build(fix): change the separator
    
    * fix: flipping the control keys
    
    * Fix: Use main branch for zmk
    
    * Fix: Use CONFIG_ZMK_POINTING for mouse keys
    
    * Fix: Remove secrets: inherit from workflow
    
    * Fix: Use full URL for ZMK branch
    
    * Revert branches to default and add secrets: inherit back
    
    * Chore: Remove push trigger from workflow

--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -5,3 +5,21 @@
 
 #define U_MT(MOD, TAP) &u_mt MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
+
+#if defined (HRM_ORDER_GASC)
+  #define U_HRM(b1, b2, b3, b4) \
+  U_MT(LGUI, b1), U_MT(LALT, b2), U_MT(LSHFT, b3), U_MT(LCTRL, b4) 
+  #define U_HRM_FLIP(b1, b2, b3, b4) \
+  U_MT(LCTRL, b1), U_MT(LSHFT, b2), U_MT(LALT, b3), U_MT(LGUI, b4)
+#else
+  #define U_HRM(b1, b2, b3, b4) \
+  U_MT(LGUI, b1), U_MT(LALT, b2), U_MT(LCTRL, b3), U_MT(LSHFT, b4)
+  #define U_HRM_FLIP(b1, b2, b3, b4) \
+  U_MT(LSHFT, b1), U_MT(LCTRL, b2), U_MT(LALT, b3), U_MT(LGUI, b4)
+#endif
+
+#define U_HRM_L(b1, b2, b3, b4) \
+U_HRM(b1, b2, b3, b4)
+
+#define U_HRM_R(b1, b2, b3, b4) \
+U_HRM_FLIP(b1, b2, b3, b4)
